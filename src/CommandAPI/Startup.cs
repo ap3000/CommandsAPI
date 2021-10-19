54bb66161a97b77d69be0e9a24144471c4728e31
Anders Pedersen <anders.pedersen@businessconcern.nl>
2021-10-19 20:30:42 +0200

    Now is working

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -48,7 +48,7 @@ namespace CommandAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env,CommandContext context)
         {
-            context.Database.Migrate();
+            //context.Database.Migrate();
             
             if (env.IsDevelopment())
             {
